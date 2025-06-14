85af91684066d2d85736d62622a2959d88499f5b
aklile-yilma <aklileyilma@gmail.com>
2025-06-15 01:59:58 +0300

    fix: build issue

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,16 @@ import express, { Request, Response, Router } from 'express';
 import cors from 'cors';
 import * as dotenv from "dotenv";
 import aiRouter from './routes/aiRoutes';
-import connectDB from './utils/connectDB';
 import equipmentRouter from './routes/equipmentRoutes';
 import failureReportRoutes from './routes/failureReportsRoutes';
 import MaintenanceHistoryRouter from './routes/maintenanceHistoryRouter';
-import { connectToMongoDB } from './db/mongodb';
-import { auth } from './utils/auth';
-import { toNodeHandler } from "better-auth/node";
-import { fromNodeHeaders } from "better-auth/node";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
 
 // Middleware
 app.use(cors({
@@ -22,28 +19,6 @@ app.use(cors({
   credentials: true,
 }));
 
-// Connect to MongoDB
-connectToMongoDB().catch(console.error);
-
-// Better Auth API routes
-app.all("/api/auth/*", toNodeHandler(auth));
-
-// Protected API example
-app.get("/api/me", async (req, res: any) => {
- 	const session = await auth.api.getSession({
-      headers: fromNodeHeaders(req.headers),
-    });
-	return res.json(session);
-});
-
-// Health check
-app.get('/health', (req, res) => {
-  res.json({ status: 'OK', service: 'Hospiwise Auth Server' });
-});
-// Connect to MongoDB
-connectDB();
-
-app.use(express.json());
 
 // Middleware
 app.use(cors({
